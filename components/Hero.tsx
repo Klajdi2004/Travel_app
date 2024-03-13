f5a3abdea8aefa85db2c00d15aa500311a231475
Klajdi2004 <klajdi.dhana1@gmail.com>
2024-03-13 07:06:47 +0100

    Added Hero, Camp, Guide, and Features components to the Home page. Added a menu icon to the Navbar for mobile devices.

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className="max-container padding-container flex flex-col
+    gap-20 py-10 pb-32 md:gap-28 lg:py-20 x1:flex-row border-2 border-red-500">
+      <div className="hero-map" />
+    </section>
+  )
+}
+
+export default Hero
