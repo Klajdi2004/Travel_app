1b0434e37f24f326187f3db76a0356a132c6f895
Klajdi2004 <klajdi.dhana1@gmail.com>
2024-03-13 07:38:31 +0100

    Update components/Camp.tsx, components/Features.tsx, components/Footer.tsx, components/GetApp.tsx, components/Guide.tsx, and components/Hero.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,7 @@ import Button from "./Button"
 
 const Navbar = () => {
     return (
-        <nav className="border-2 border-red-500 flexBetween max-container
-        padding-container relative z-30 py-5">
+        <nav className="flexBetween max-container padding-container relative z-30 py-5">
             <Link href="/">
                 <Image src="/hilink-logo.svg" alt="logo" width={74} 
                 height={29} />
